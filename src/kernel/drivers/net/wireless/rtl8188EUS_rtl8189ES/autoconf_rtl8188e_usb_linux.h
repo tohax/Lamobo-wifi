06475e522dc5c813f36c142ef1b5ef94c1f6fceb
tohax <tohax@mail.ru>
2018-05-23 14:07:35 +0500

    modified:   src/kernel/drivers/net/wireless/rtl8188EUS_rtl8189ES/autoconf_rtl8188e_usb_linux.h 	modified:   src/kernel/drivers/net/wireless/rtl8188EUS_rtl8189ES/include/autoconf.h

--- src/kernel/drivers/net/wireless/rtl8188EUS_rtl8189ES/autoconf_rtl8188e_usb_linux.h
+++ src/kernel/drivers/net/wireless/rtl8188EUS_rtl8189ES/autoconf_rtl8188e_usb_linux.h
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  * Copyright(c) 2007 - 2011 Realtek Corporation. All rights reserved.
- *                                        
+ *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of version 2 of the GNU General Public License as
  * published by the Free Software Foundation.
@@ -33,24 +33,22 @@
 #define RTL871X_MODULE_NAME "88EU"
 #define DRV_NAME "rtl8188eu"
 
-#define CONFIG_USB_HCI	
+#define CONFIG_USB_HCI
+#define CONFIG_RTL8188E
 
-#define CONFIG_RTL8188E	
-
-#define PLATFORM_LINUX	
-
-//#define CONFIG_IOCTL_CFG80211 
+#define PLATFORM_LINUX
+//#define CONFIG_IOCTL_CFG80211
 
 #if defined( CONFIG_PLATFORM_ARM_SUNxI) || defined(CONFIG_PLATFORM_ACTIONS_ATM702X)
-	#ifndef CONFIG_IOCTL_CFG80211 
-		#define CONFIG_IOCTL_CFG80211 
+	#ifndef CONFIG_IOCTL_CFG80211
+		#define CONFIG_IOCTL_CFG80211
 	#endif
 #endif
 
 #ifdef CONFIG_IOCTL_CFG80211
 	//#define RTW_USE_CFG80211_STA_EVENT /* Indecate new sta asoc through cfg80211_new_sta */
 	#define CONFIG_CFG80211_FORCE_COMPATIBLE_2_6_37_UNDER
-	//#define CONFIG_DEBUG_CFG80211 
+	//#define CONFIG_DEBUG_CFG80211
 	//#define CONFIG_DRV_ISSUE_PROV_REQ // IOT FOR S2
 	#define CONFIG_SET_SCAN_DENY_TIMER
 
@@ -62,38 +60,38 @@
 
 //#define CONFIG_H2CLBK
 
-#define CONFIG_EMBEDDED_FWIMG	
+#define CONFIG_EMBEDDED_FWIMG
 //#define CONFIG_FILE_FWIMG
 
 #define CONFIG_XMIT_ACK
 #ifdef CONFIG_XMIT_ACK
 	#define CONFIG_ACTIVE_KEEP_ALIVE_CHECK
 #endif
-#define CONFIG_80211N_HT	
+#define CONFIG_80211N_HT
 
-#define CONFIG_RECV_REORDERING_CTRL	
+#define CONFIG_RECV_REORDERING_CTRL
 
-//#define CONFIG_TCP_CSUM_OFFLOAD_RX	
+//#define CONFIG_TCP_CSUM_OFFLOAD_RX
 
-//#define CONFIG_DRVEXT_MODULE	
+//#define CONFIG_DRVEXT_MODULE
 
  #define CONFIG_SUPPORT_USB_INT
  #ifdef	CONFIG_SUPPORT_USB_INT
-//#define CONFIG_USB_INTERRUPT_IN_PIPE	
+//#define CONFIG_USB_INTERRUPT_IN_PIPE
 #endif
 
 //#ifndef CONFIG_MP_INCLUDED
-	#define CONFIG_IPS	
+	#define CONFIG_IPS
 	#ifdef CONFIG_IPS
 	//#define CONFIG_IPS_LEVEL_2	 //enable this to set default IPS mode to IPS_LEVEL_2
 	#endif
-	#define SUPPORT_HW_RFOFF_DETECTED	
+	#define SUPPORT_HW_RFOFF_DETECTED
 
-	#define CONFIG_LPS	
+	#define CONFIG_LPS
 	#if defined(CONFIG_LPS) && defined(CONFIG_SUPPORT_USB_INT)
 
 
-	//#define CONFIG_LPS_LCLK	
+	//#define CONFIG_LPS_LCLK
 	#endif
 
 	#ifdef CONFIG_LPS_LCLK
@@ -104,12 +102,12 @@
 	#define CONFIG_ANTENNA_DIVERSITY
 
 	//after link
-	#ifdef CONFIG_ANTENNA_DIVERSITY	 
-	#define CONFIG_HW_ANTENNA_DIVERSITY		
+	#ifdef CONFIG_ANTENNA_DIVERSITY
+	#define CONFIG_HW_ANTENNA_DIVERSITY
 	#endif
 
 
-	//#define CONFIG_CONCURRENT_MODE 
+	//#define CONFIG_CONCURRENT_MODE
 	#ifdef CONFIG_CONCURRENT_MODE
 		//#define CONFIG_HWPORT_SWAP				//Port0->Sec , Port1 -> Pri
 		#define CONFIG_TSF_RESET_OFFLOAD 			// For 2 PORT TSF SYNC.
@@ -117,10 +115,10 @@
 
 	#define CONFIG_IOL
 //#else 	//#ifndef CONFIG_MP_INCLUDED
-	
+
 //#endif 	//#ifndef CONFIG_MP_INCLUDED
 
-#define CONFIG_AP_MODE	
+#define CONFIG_AP_MODE
 #ifdef CONFIG_AP_MODE
 	//#define CONFIG_INTERRUPT_BASED_TXBCN // Tx Beacon when driver BCN_OK ,BCN_ERR interrupt occurs
 	#if defined(CONFIG_CONCURRENT_MODE) && defined(CONFIG_INTERRUPT_BASED_TXBCN)
@@ -128,22 +126,22 @@
 	#endif
 	#ifdef CONFIG_INTERRUPT_BASED_TXBCN
 		//#define CONFIG_INTERRUPT_BASED_TXBCN_EARLY_INT
-		#define CONFIG_INTERRUPT_BASED_TXBCN_BCN_OK_ERR		
+		#define CONFIG_INTERRUPT_BASED_TXBCN_BCN_OK_ERR
 	#endif
-	
+
 	#define CONFIG_NATIVEAP_MLME
 	#ifndef CONFIG_NATIVEAP_MLME
-		#define CONFIG_HOSTAPD_MLME	
-	#endif			
-	#define CONFIG_FIND_BEST_CHANNEL	
-	//#define CONFIG_NO_WIRELESS_HANDLERS	
+		#define CONFIG_HOSTAPD_MLME
+	#endif
+	#define CONFIG_FIND_BEST_CHANNEL
+	//#define CONFIG_NO_WIRELESS_HANDLERS
 #endif
 
-#define CONFIG_P2P	
+#define CONFIG_P2P
 #ifdef CONFIG_P2P
 	//The CONFIG_WFD is for supporting the Wi-Fi display
 	#define CONFIG_WFD
-	
+
 	#ifndef CONFIG_WIFI_TEST
 		#define CONFIG_P2P_REMOVE_GROUP_INFO
 	#endif
@@ -154,13 +152,13 @@
 #endif
 
 //	Added by Kurt 20110511
-//#define CONFIG_TDLS	
+//#define CONFIG_TDLS
 #ifdef CONFIG_TDLS
 //	#ifndef CONFIG_WFD
-//		#define CONFIG_WFD	
+//		#define CONFIG_WFD
 //	#endif
-//	#define CONFIG_TDLS_AUTOSETUP			
-//	#define CONFIG_TDLS_AUTOCHECKALIVE		
+//	#define CONFIG_TDLS_AUTOSETUP
+//	#define CONFIG_TDLS_AUTOCHECKALIVE
 #endif
 
 
@@ -179,9 +177,9 @@
 	#define CONFIG_IOL_READ_EFUSE_MAP
 	//#define DBG_IOL_READ_EFUSE_MAP
 	//#define CONFIG_IOL_LLT
-	#define CONFIG_IOL_EFUSE_PATCH		
+	#define CONFIG_IOL_EFUSE_PATCH
 	//#define CONFIG_IOL_IOREG_CFG
-	//#define CONFIG_IOL_IOREG_CFG_DBG	
+	//#define CONFIG_IOL_IOREG_CFG_DBG
 #endif
 
 
@@ -207,16 +205,16 @@
 #define CONFIG_CHECK_AC_LIFETIME 	// Check packet lifetime of 4 ACs.
 
 
-/* 
- * Interface  Related Config 
+/*
+ * Interface  Related Config
  */
 
 #ifndef CONFIG_MINIMAL_MEMORY_USAGE
-	#define CONFIG_USB_TX_AGGREGATION	
-	#define CONFIG_USB_RX_AGGREGATION	
+	#define CONFIG_USB_TX_AGGREGATION
+	#define CONFIG_USB_RX_AGGREGATION
 #endif
 
-#define CONFIG_PREALLOC_RECV_SKB	
+#define CONFIG_PREALLOC_RECV_SKB
 //#define CONFIG_REDUCE_USB_TX_INT		// Trade-off: Improve performance, but may cause TX URBs blocked by USB Host/Bus driver on few platforms.
 //#define CONFIG_EASY_REPLACEMENT	
 
@@ -365,14 +363,14 @@
  */
 #define DBG	1
 
-#define CONFIG_DEBUG /* DBG_871X, etc... */
-#define CONFIG_DEBUG_RTL871X /* RT_TRACE, RT_PRINT_DATA, _func_enter_, _func_exit_ */
+//#define CONFIG_DEBUG /* DBG_871X, etc... */
+//#define CONFIG_DEBUG_RTL871X /* RT_TRACE, RT_PRINT_DATA, _func_enter_, _func_exit_ */
 
 #define CONFIG_PROC_DEBUG
 
-//#define DBG_CONFIG_ERROR_DETECT
+#define DBG_CONFIG_ERROR_DETECT
 //#define DBG_CONFIG_ERROR_DETECT_INT
-//#define DBG_CONFIG_ERROR_RESET
+#define DBG_CONFIG_ERROR_RESET
 
 //#define DBG_IO
 //#define DBG_DELAY_OS
